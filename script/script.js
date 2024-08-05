8399883f642f6f0332a86099ded7c1b1341eedbe
Javier <javieralzuu@gmail.com>
2024-08-05 06:04:24 -0300

    first cmmit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+let navBar = document.getElementById('nav-list');
+let toggleMenu = document.getElementById('nav-menu');
+
+toggleMenu.addEventListener('click', () => {
+    navBar.classList.toggle('show-nav')
+    
+}
+
+)
